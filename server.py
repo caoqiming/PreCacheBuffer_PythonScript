49804847e7e69c48c2de8a8bfc52a2ad46cdea25
caoqiming <caoqiming@bupt.edu.cn>
2021-11-01 20:43:04 +0800

    prepare

--- server.py
+++ server.py
@@ -10,10 +10,10 @@ n=3952 #电影的个数
 #用户数 电影数 用户额外信息 电影额外信息 隐藏层 l1层
 model=CFA(6040,3952,29,18,64,128)
 
-model.encoder_user.load_weights("./model/encoder_user.h5")
-model.decoder_user.load_weights("./model/decoder_user.h5")
-model.encoder_movie.load_weights("./model/encoder_movie.h5")
-model.decoder_movie.load_weights("./model/decoder_movie.h5")
+# model.encoder_user.load_weights("./model/encoder_user.h5")
+# model.decoder_user.load_weights("./model/decoder_user.h5")
+# model.encoder_movie.load_weights("./model/encoder_movie.h5")
+# model.decoder_movie.load_weights("./model/decoder_movie.h5")
 
 with open("data/image.txt","r") as f:
     urls=f.readlines()
@@ -73,4 +73,3 @@ if __name__ == '__main__':
     print("Starting server, listen at: %s:%s" % host)
     server.serve_forever()
 
-# %%
